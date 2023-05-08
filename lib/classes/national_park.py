c7dd9fd38c021de7d1ccbd4ad05c2c6dcc42bcf9
+ <+>
2023-05-08 17:09:31 -0400

    satisft deliverables

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -4,7 +4,13 @@ class NationalPark:
         self.name = name
         self._trips = []
         self._visitors = []
-
+        self.visitors_count = {}
+        
+    def __repr__(self):
+        return f'''
+                    name: {self.name}
+                '''
+        
     @property
     def name(self):
         return self._name
@@ -14,29 +20,39 @@ class NationalPark:
         if isinstance(name, str) and not hasattr(self, 'name'):
             self._name = name
         else:
-            raise Exception
+            raise Exception("Name must not exist and must be a string")
         
     def trips(self, new_trip=None):
         from classes.trip import Trip
         if new_trip and isinstance(new_trip, Trip):
             self._trips.append(new_trip)
+            
         return self._trips
+        
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
+        
         if new_visitor and isinstance(new_visitor, Visitor) and new_visitor not in self._visitors:
             self._visitors.append(new_visitor)
+            
+        #     self.visitors_count[new_visitor] = 1
+        
+        # elif isinstance(new_visitor, Visitor): 
+        #     self.visitors_count[new_visitor] += 1
+            
         return self._visitors
+        
     
     def total_visits(self):
         return len(self._trips)
     
     def best_visitor(self):
-        max_visitor = 0
-        max_visits = 0
-        for v in self._visitors:
-            v_visits = len([t for t in self._trips if t.visitor == v])
-            if v_visits > max_visits:
-                max_visits = v_visits
-                max_visitor = v
-        return max_visitor
+        # return max(self.visitors_count, key = self.visitors_count.get)
+        for trip in self._trips:
+            # looping over the trips
+            if trip.visitor in self.visitors_count:
+                self.visitors_count[trip.visitor] += 1
+            else: 
+                self.visitors_count[trip.visitor] = 1
+        return max(self.visitors_count, key=self.visitors_count.get)
--- lib/classes/visitor.py
+++ lib/classes/visitor.py
@@ -1,29 +1,45 @@
+import ipdb
 class Visitor:
 
     def __init__(self, name):
         self.name = name
         self._trips = []
         self._national_parks = []
-
-    @property
-    def name(self):
+        
+    def __repr__(self):
+        return f'''
+                name: {self.name}
+                '''
+        
+    # @property
+    def get_name(self): 
         return self._name
     
-    @name.setter
-    def name(self, name=None):
-        if isinstance(name, str) and not hasattr(self, 'name'):
-            self._name = name
-        else:
-            raise Exception
+    # @name.setter
+    def set_name(self, name):
+        # must be a str
+        # must be between 1 to 15 characters
+        # cannot be changed once it has been initialized
         
-    def trips(self, new_trip):
+        if type(name) == str and len(name) >= 1 and len(name) <= 15 and not hasattr(self, 'name'):
+            self._name = name
+            
+        else:
+            raise Exception("Name is not a string or not between 1 to 15 characters or name already exists")
+        
+    def trips(self, new_trip=None):
         from classes.trip import Trip
         if new_trip and isinstance(new_trip, Trip):
             self._trips.append(new_trip)
+        
         return self._trips
     
     def national_parks(self, new_national_park=None):
         from classes.national_park import NationalPark
         if new_national_park and isinstance(new_national_park, NationalPark) and new_national_park not in self._national_parks:
             self._national_parks.append(new_national_park)
+            
         return self._national_parks
+        
+
+    name = property(get_name, set_name)
